d05393c55b1a5921cf96b8abb2348ccdf9e86282
Po <po.heize@gmail.com>
2021-05-04 08:26:53 +0300

    console.log

--- script.js
+++ script.js
@@ -6,7 +6,21 @@ Test console
 
 
 let number = 10;
+let stringType = "string";
+let boolValue = false;
+let numberArray = [20, 10, 30];
 
 
+number = 10;
 
-console.log(number)
+if (number == 10){
+
+    console.log("number is 10");
+
+} else if(number == 20){
+
+    console.log("number is 10 * 2");
+} else {
+
+    console.log("number is not 10")
+}
