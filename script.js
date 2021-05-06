a7e8c968d40c3325dfdcda4f9fef3bfb6d186bb7
Po <po.heize@gmail.com>
2021-05-06 18:42:09 +0300

    papildyta

--- script.js
+++ script.js
@@ -5,21 +5,23 @@ Test console
 
 
 
-let number = 10;
+
 let stringType = "string";
 let boolValue = false;
 let numberArray = [20, 10, 30];
 
+let numberString = prompt('Please enter a number');
 
-number = 10;
+let number = parseInt(numberString);
+
 
 if (number == 10){
 
-    console.log("number is 10");
+    console.log("number is 10") 
 
 } else if(number == 20){
 
-    console.log("number is 10 * 2");
+    console.log("number is 10 * 2")
 } else {
 
     console.log("number is not 10")
